399b0e6b0cf08d5e25c7743f5564725362c6fc72
Souvik Mondal <souvik.mondal@thermofisher.com>
2025-05-18 19:21:06 +0530

    added  parcel

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-const header = React.createElement("h1", {}, "Hi jim is coding now");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const header = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Hello World"),
+  React.createElement("h2", {}, "This is Jim the developer.")
+);
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 rootElement.render(header);
